e46c845e4dc16dabb8ea41c0c8ddf457132c1e97
王兴宇 <666wxy666@gmail.com>
2020-04-13 09:50:53 +0800

    北邮android课程第四次作业，模拟时钟app

--- assignment/Clock/app/src/main/java/com/bytedance/clockapplication/widget/Clock.java
+++ assignment/Clock/app/src/main/java/com/bytedance/clockapplication/widget/Clock.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import java.util.Calendar;
@@ -105,8 +106,8 @@ public class Clock extends View {
         drawHoursValues(canvas);
         drawNeedles(canvas);
 
-        // todo 每一秒刷新一次，让指针动起来
-
+        // done 每一秒刷新一次，让指针动起来
+        postInvalidateDelayed(1000);
     }
 
     private void drawDegrees(Canvas canvas) {
@@ -166,7 +167,8 @@ public class Clock extends View {
         // 画秒针
         drawPointer(canvas, 2, nowSeconds);
         // 画分针
-        // todo 画分针
+        // done 画分针
+        drawPointer(canvas, 1, nowMinutes);
         // 画时针
         int part = nowMinutes / 12;
         drawPointer(canvas, 0, 5 * nowHours + part);
@@ -188,13 +190,18 @@ public class Clock extends View {
                 pointerHeadXY = getPointerHeadXY(HOUR_POINTER_LENGTH, degree);
                 break;
             case 1:
-                // todo 画分针，设置分针的颜色
-
+                // done 画分针，设置分针的颜色
+                degree=value*UNIT_DEGREE;
+                mNeedlePaint.setColor(Color.BLUE);
+                pointerHeadXY = getPointerHeadXY(MINUTE_POINTER_LENGTH, degree);
                 break;
             case 2:
                 degree = value * UNIT_DEGREE;
                 mNeedlePaint.setColor(Color.GREEN);
                 pointerHeadXY = getPointerHeadXY(SECOND_POINTER_LENGTH, degree);
+                break;
+            default:
+                Log.d("ERROR:","switch error");
                 break;
         }
 
